0652cd3ea177d305860ddd9b5796a3a2b724a8f6
Chinmay Wyawahare <chinmaynw@gmail.com>
2019-02-13 22:27:04 -0500

    Fixed response URL

--- viz.py
+++ viz.py
@@ -3,8 +3,9 @@ import requests
 # making a search request to the Spotify API to return
 # any artist with 'OneRepublic' in their name
 #response = requests.get('https://api.spotify.com/v1/search?query=OneRepublic&type=artist&limit=50&market=US')
-response = requests.get('https://api.spotify.com/v1/search?q=dua%20lipa&type=artist&limit=50')
-response = requests.get('https://api.spotify.com/v1/artists/5Pwc4xIPtQLFEnJriah9YJ/top-tracks?country=SE')
+#response = requests.get('https://api.spotify.com/v1/search?q=dua%20lipa&type=artist&limit=50')
+response = requests.get('https://api.spotify.com/v1/search?q=one&type=artist&limit=50')
+#response = requests.get('https://api.spotify.com/v1/artists/5Pwc4xIPtQLFEnJriah9YJ/top-tracks?country=SE')
 
 # converts the response from a string to a json object that can be parsed in Python.
 data = response.json()
@@ -13,6 +14,7 @@ data = response.json()
 # https://api.spotify.com/v1/artists/5Pwc4xIPtQLFEnJriah9YJ/top-tracks?country=SE
 # looking at the 'items' returned for 'artists' in this request
 
-lil_artists = data['artists']['items'] 
+#lil_artists = data['artists']['items'] 
+print(data)
 # iterating through each artist and their items
 # items contains the information we would like to recieve for each artist.
